287da26be8cdc515ede70bac3a6f91c91b49a741
DOLAPO <mac@MACs-MacBook-Pro.local>
2024-03-06 10:20:22 -0800

    update

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Enable CORS for all routes
 app.use(cors({
-    origin: 'https://conf1rmation.vercel.app/',
+    origin: 'https://confirmation-sweg.onrender.com/',
     credentials: true,
 }));
 

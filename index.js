1898629182487ca6a633ceb14d0416fed5c8bad5
DOLAPO <mac@MACs-MacBook-Pro.local>
2024-03-06 09:51:01 -0800

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const axios = require('axios');
 const app = express();
 const path = require('path');
@@ -21,14 +21,20 @@ app.use(express.static(path.join(__dirname, 'images')));
     res.sendFile(path.join(__dirname, 'index.html'));
   });
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 // Enable CORS for all routes
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:8000/',
+    credentials: true,
+}));
 
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/', (req, res) => {
     
